4311b6f10dd7d1bcf5bc591c52f476ab3b3ec487
jeong5728 <jeong5728@gmail.com>
2022-10-14 19:51:19 +0900

    04 예제 추가

---
+++ 04-move-direction/board.js
@@ -0,0 +1,36 @@
+export default class Board {
+  constructor(game){
+    this.game = game;
+
+    this.image = new Image();
+    this.image.src = './tiles.png';
+  }
+
+  update(){}
+
+  draw(){
+    const { map, context, tileSize, player, $canvas } = this.game;
+    const uy = player.canvasPos.y; // user y, x
+    const ux = player.canvasPos.x;
+    const ch = $canvas.height; // canvas height, width
+    const cw = $canvas.width;
+    const baseX = (cw/2 - ux) - tileSize/2;
+    const baseY = (ch/2 - uy) - tileSize/2;
+
+    map.forEach((row, y) => {
+      row.forEach((col, x) => {
+        const bgImageCoord = map[y][x]
+          ? [32, 0, 32, 32]
+          : [0, 0, 32, 32]
+        context.drawImage(
+          this.image,
+          ...bgImageCoord,
+          baseX + x*tileSize,
+          baseY + y*tileSize,
+          tileSize,
+          tileSize,
+        );
+      });
+    })
+  }
+}
---
+++ 04-move-direction/main.js
@@ -0,0 +1,58 @@
+import InputHandler from './input.js';
+import Player from './player.js';
+import Board from './board.js';
+
+class Game {
+  constructor(){
+    this.$canvasWrapper = document.querySelector('#canvas-wrapper');
+    this.$canvas = this.$canvasWrapper.querySelector('canvas');
+    this.context = this.$canvas.getContext('2d');
+
+    this.tileSize = 40;
+
+    this.map = [ // 1: 벽, 0: 땅
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 0, 0],
+      [0, 0, 1, 1, 0],
+      [0, 0, 0, 0, 0],
+    ];
+
+    this.player = new Player(this);
+    this.board = new Board(this);
+    this.input = new InputHandler();
+
+    this.equalizeElementSize(this.$canvas, this.$canvasWrapper);
+    window.addEventListener('resize', () => {
+      this.equalizeElementSize(this.$canvas, this.$canvasWrapper);
+    });
+
+    this.animate();
+  }
+
+  update(){
+    this.board.update();
+    this.player.update();
+  }
+
+  draw(){
+    this.board.draw();
+    this.player.draw();
+  }
+
+  animate = () => {
+    const { width, height } = this.$canvas;
+    this.context.clearRect(0, 0, width, height);
+    this.update();
+    this.draw();
+    requestAnimationFrame(this.animate);
+  }
+
+  equalizeElementSize($target, $element){
+    $target.width = $element.offsetWidth;
+    $target.height = $element.offsetHeight;
+  }
+}
+
+window.addEventListener('load', () => {
+  new Game();
+});
